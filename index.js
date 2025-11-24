acac6f1b04a3f1b198eb911d8f82d706561774ae
SupAdri <adrian2005hc@gmail.com>
2025-11-24 18:06:13 -0500

    a ver si no va a salir el index

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const http = require('http');
 const WebSocket = require('ws');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
 // Servir archivos estáticos (cliente)
-app.use(express.static('public'));
+//app.use(express.static('public'));
 
 wss.on('connection', (ws) => {
   console.log('Cliente conectado');
@@ -34,7 +35,9 @@ wss.on('connection', (ws) => {
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Servidor escuchando en http://localhost:${PORT}`));
 
-
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 // const express = require('express');
 
