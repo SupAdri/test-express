6df92980d7bde48722e78d86201fe4626c009ec1
SupAdri <adrian2005hc@gmail.com>
2025-11-24 15:57:35 -0500

    probando cositas

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ app.get('/api', (req, res) => {
   res.send(pinga);
 });
 
+app.get('/api/mas', (req, res) => {
+  pinga.gei = 'nooooooo'
+  res.send(pinga);
+});
+
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en http://localhost:${PORT}`);
 });
